138247185f04889183c99013f256e876fbc9bf9e
AjiriOgeh <151365920+AjiriOgeh@users.noreply.github.com>
2024-08-24 18:37:36 +0100

    First commit for landing page

---
+++ src/pages/landingPage/LandingPage.jsx
@@ -0,0 +1,137 @@
+import style from "./index.module.css"
+import logo from "../../landingpageassets/Taskiro-logo.png"
+
+const LandingPage = () => {
+    return (
+        <>
+            <div className={style.container}>
+
+                <div className={style.navBar}>
+                    <div>
+                        <img src={logo}  alt={"Logo"}/>
+                    </div>
+                    <div className={style.rightSideNavBar}>
+                        <button className={style.navBarButtonText} >Home</button>
+                        <button>Features</button>
+                        <button>Pricing</button>
+                        <button>Contact</button>
+                        <button>Login</button>
+                        <button className={style.signUp}>Sign up</button>
+                    </div>
+                </div>
+
+                <div>
+
+                    <div>
+                        <p>Cassez la routine avec <span>Taskiro</span></p>
+                        <p>Taskiro vous aide a rendre des taches et projet plus interessant, tout en respectant vos
+                            habitudes</p>
+                    </div>
+
+                    <div>
+                        <button>Decouvir taskiro</button>
+                        <button>Deja membre</button>
+                    </div>
+
+                    <div>
+                        images main
+                    </div>
+
+                    <div>
+                        <div>
+                            image
+                        </div>
+                        <div>
+                            <p>Centralisation</p>
+                            <p>Gerez votre aventure de A a Z</p>
+                            <p>Plainifiez, notifez, confiez, gerez toutes les facettes de vos projets a partir d un seul
+                                outil complet et facile a prendre en main</p>
+                            <button>Fonctionalites</button>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div>
+                            image
+                        </div>
+                        <div>
+                            <p>Centralisation</p>
+                            <p>Gerez votre aventure de A a Z</p>
+                            <p>Plainifiez, notifez, confiez, gerez toutes les facettes de vos projets a partir d un seul
+                                outil complet et facile a prendre en main</p>
+                            <button>Fonctionalites</button>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div>
+                            image
+                        </div>
+                        <div>
+                            <p>Centralisation</p>
+                            <p>Gerez votre aventure de A a Z</p>
+                            <p>Plainifiez, notifez, confiez, gerez toutes les facettes de vos projets a partir d un seul
+                                outil complet et facile a prendre en main</p>
+                            <button>Fonctionalites</button>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div>
+                            image
+                        </div>
+                        <div>
+                            <p>Centralisation</p>
+                            <p>Gerez votre aventure de A a Z</p>
+                            <p>Plainifiez, notifez, confiez, gerez toutes les facettes de vos projets a partir d un seul
+                                outil complet et facile a prendre en main</p>
+                            <button>Fonctionalites</button>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div>
+                            image
+                        </div>
+                        <div>
+                            <p>Centralisation</p>
+                            <p>Gerez votre aventure de A a Z</p>
+                            <p>Plainifiez, notifez, confiez, gerez toutes les facettes de vos projets a partir d un seul
+                                outil complet et facile a prendre en main</p>
+                            <button>Fonctionalites</button>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div>
+                            image
+                        </div>
+                        <div>
+                            <p>Centralisation</p>
+                            <p>Gerez votre aventure de A a Z</p>
+                            <p>Plainifiez, notifez, confiez, gerez toutes les facettes de vos projets a partir d un seul
+                                outil complet et facile a prendre en main</p>
+                            <button>Fonctionalites</button>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div>
+                            image
+                        </div>
+                        <div>
+                            <p>Centralisation</p>
+                            <p>Gerez votre aventure de A a Z</p>
+                            <p>Plainifiez, notifez, confiez, gerez toutes les facettes de vos projets a partir d un seul
+                                outil complet et facile a prendre en main</p>
+                            <button>Fonctionalites</button>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </>
+    );
+}
+export default LandingPage;
