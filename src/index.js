09c5e2547515db88602a49401e89cd80af94d77b
aramide <aramideotenaike@gmail.com>
2024-09-06 06:59:36 +0100

    removed chakra

--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import theme from "./chakra/theme";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
         <Provider store={store}>
-            {/*<ChakraProvider theme={theme}>*/}
-            {/*    <ColorModeScript initialColorMode={theme.config.initialColorMode} />*/}
                 <App />
-            {/*</ChakraProvider>*/}
         </Provider>
 );
 
