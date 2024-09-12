f02e5e5922f4b0cd8bbfc7391b8c8d3b9d853778
Aramide <aramideotenaike@gmail.com>
2024-09-12 09:50:41 +0100

    create project form

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,11 +1,11 @@
 import React, { createContext, useState, useContext } from 'react';
 
-export const UserContext = createContext();
+export const UserContext = createContext("");
 
 export const UserProvider = ({ children }) => {
     const [user, setUser] = useState({
-        name: '',
         email: '',
+        fullName: '',
         avatar: '../assets/defaultAvatar.png',
     });
 
--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Provider } from "react-redux";
-import store from "./redux/store";
+import {UserProvider} from './context/UserContext';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-        <Provider store={store}>
+        <UserProvider>
                 <App />
-        </Provider>
+        </UserProvider>
 );
 
 reportWebVitals();
