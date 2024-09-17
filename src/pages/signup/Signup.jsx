7baaa850efe1600c9fbb9f9807db1bf8f3e577d7
Aramide <aramideotenaike@gmail.com>
2024-09-17 10:27:23 +0100

    error message

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -54,7 +54,8 @@ const Signup = () => {
                 setSubmitStatus({ error: response.data?.message || "Something went wrong. Please try again." });
             }
         } catch (error) {
-            setSubmitStatus({ error: error.response?.data?.message || "An error occurred during signup." });
+            console.log("error: ", error)
+            setSubmitStatus({ error: error.response.data });
         } finally {
             setSubmitting(false);
         }
